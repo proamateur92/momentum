791f89ff5ed4df1140dc630bea4ade240fe235f1
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-03-21 06:08:46 +0900

    JS: updated img file path

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 const image = ['0.jpg', '1.jpg', '2.jpg'];
 const imageName = Math.floor(Math.random() * image.length);
 const cover = document.querySelector('.cover');
-cover.style.backgroundImage = `url('../img/${image[imageName]}')`;
+cover.style.backgroundImage = `url('./img/${image[imageName]}')`;
